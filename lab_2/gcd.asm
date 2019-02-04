5a6ca6e7dd6f51c89f399e682aa809d822d3b2fd
Sirakorn Lamyai <tanersirakorn@gmail.com>
2019-02-04 13:17:29 +0700

    Converting indentation to tabs

--- lab_2/gcd.asm
+++ lab_2/gcd.asm
@@ -1,5 +1,5 @@
 .data
-    text_a: .asciiz "gcd of 1890 and 3315 is "
+	text_a: .asciiz "gcd of 1890 and 3315 is "
 
 .text
 
@@ -14,14 +14,14 @@ main:
 	li  	$t4, 3315
 	sw  	$t4, 0($sp)
 	jal 	gcd
-    add     $s0, $0, $v0
+	add     $s0, $0, $v0
 	addiu   $sp, $sp, 8
-    li      $v0, 4
-    la		$a0, text_a 
-    syscall
-    li      $v0, 1
-    add     $a0, $0, $s0
-    syscall
+	li      $v0, 4
+	la		$a0, text_a 
+	syscall
+	li      $v0, 1
+	add     $a0, $0, $s0
+	syscall
 	li	    $v0, 10
 	syscall	
 
@@ -35,43 +35,43 @@ gcd:
 	lw	    $a0, 8($fp)     # loading 1st args (m)
 	lw	    $a1, 12($fp)    # loading 2nd args (n)
 
-    beq     $a0, $a1, gcd_terminates
-    slt     $t0, $a1, $a0
-    bne     $t0, $0, gcd_mgtn
-    beq     $t0, $0, gcd_mleqn
+	beq     $a0, $a1, gcd_terminates
+	slt     $t0, $a1, $a0
+	bne     $t0, $0, gcd_mgtn
+	beq     $t0, $0, gcd_mleqn
 
 gcd_terminates:
-    add		$v0, $0, $a0	# loads $m$ into return value
+	add		$v0, $0, $a0	# loads $m$ into return value
 	lw	    $fp, 0($sp)     # popping back the current frame pointer
 	addiu	$sp, $sp, 4
 	lw	    $ra, 0($sp)     # popping back the return address
 	addiu	$sp, $sp, 4     # summation to the originated stack base
-    jr      $ra             # jr to return
+	jr      $ra             # jr to return
 
 gcd_mgtn:
 	addiu	$sp, $sp, -4    # decrease stack pointer by 4
-    sw      $a1, 0($sp)     # stores $n$
+	sw      $a1, 0($sp)     # stores $n$
 	addiu	$sp, $sp, -4    # decrease stack pointer by 4
-    sub		$s0, $a0, $a1   # $s0 = $m-n$
-    sw      $s0, 0($sp)     # stores $m-n$
+	sub		$s0, $a0, $a1   # $s0 = $m-n$
+	sw      $s0, 0($sp)     # stores $m-n$
 	jal 	gcd
-    add     $sp, $sp, 8     # pops 2 arguments (m, n)
+	add     $sp, $sp, 8     # pops 2 arguments (m, n)
 	lw	    $fp, 0($sp)     # popping back the current frame pointer
 	addiu	$sp, $sp, 4
 	lw	    $ra, 0($sp)     # popping back the return address
 	addiu	$sp, $sp, 4     # summation to the originated stack base
-    jr      $ra             # jr to return
+	jr      $ra             # jr to return
 
 gcd_mleqn:
 	addiu	$sp, $sp, -4    # decrease stack pointer by 4
-    sub		$s0, $a1, $a0   # $s0 = $n-m$
-    sw      $s0, 0($sp)     # stores $n-m$
+	sub		$s0, $a1, $a0   # $s0 = $n-m$
+	sw      $s0, 0($sp)     # stores $n-m$
 	addiu	$sp, $sp, -4    # decrease stack pointer by 4
-    sw      $a0, 0($sp)     # stores $m$
+	sw      $a0, 0($sp)     # stores $m$
 	jal 	gcd
-    add     $sp, $sp, 8     # pops 2 arguments (m, n)
+	add     $sp, $sp, 8     # pops 2 arguments (m, n)
 	lw	    $fp, 0($sp)     # popping back the current frame pointer
 	addiu	$sp, $sp, 4
 	lw	    $ra, 0($sp)     # popping back the return address
 	addiu	$sp, $sp, 4     # summation to the originated stack base
-    jr      $ra             # jr to return
+	jr      $ra             # jr to return
