32d7d101c40c5c34ed08792f1188f50a5f59ca78
Sirakorn Lamyai <tanersirakorn@gmail.com>
2019-02-04 10:33:30 +0700

    :beers:

--- lab_1/CountingSort.asm
+++ lab_1/CountingSort.asm
@@ -3,7 +3,9 @@
 
 # our input is 46 characters in length
 input:  .ascii "cadljgarhtoxAHdgdsJKhYEasduwBRLsdgHoptxnaseurh"
-keys:  .word 0:512
+output_lead: .ascii "Sorted string is "
+output: .byte '0':46
+keys:  .word 0:46
 
 .text
 
@@ -15,28 +17,20 @@ main:
 
 count:
     lb      $t1, input($t0)     # t1 = input[i]
-    li	    $v0, 11
-    add     $a0, $0, $t1
-    syscall
     sll     $t1, $t1, 2         # converts t1 to actual word position (<< 2)
-    li	    $v0, 1
-    add     $a0, $0, $t1
-    syscall
     lw      $t2, keys($t1)      # load the current count
     addi    $t2, $t2, 1         # increment by one
     sw      $t2, keys($t1)      # store the cont
     addi	$t0, $t0, 1			# i++
-    bne     $t0, $t7, count     # for(..., i < count, )
+    bne     $t0, $t7, count     # for(..., i < count, ...)
 
 cumu_init:
-    li      $2, 10
-    syscall
-    addi    $t0, $0, 1          # for i (t0) = 1
-    addi    $t6, $0, 128        # t6 = 128 (ascii's length)
+    li      $t0, 1              # for i (t0) = 1
+    li      $t6, 256            # $t6 = 128 (ascii's length)
 
 cumu:
-    addi    $t1, $t0, -1        # t1 =  i-1
-    sll     $t0, $t1, 2         # convert both (i) and (i-1) to word position
+    addi    $t1, $t0, -1        # $t1 =  i-1
+    sll     $t0, $t0, 2         # convert both (i) and (i-1) to word position
     sll     $t1, $t1, 2
     lw		$t3, keys($t0)		# load count[i]
     lw		$t4, keys($t1)		# load count[i-1]
@@ -45,6 +39,29 @@ cumu:
     srl     $t0, $t0, 2         # shifting i back from position to i
     addi    $t0, $t0, 1         # for(..., ..., i++)
     bne     $t0, $t6, cumu      # for(..., i < 128, ...)
+
+buildres_init:
+    li      $t0, 0              # for i = 0
+
+buildres:
+    lb		$t1, input($t0)		# $t1 = input[i]
+    sll     $t4, $t1, 2         # converting $t1 to word position
+    lw		$t2, keys($t4)		# $t2 = count[input[i]] - 1
+    addi    $t2, $t2, -1
+    sb		$t1, output($t2)	# output[$t2] = input[i]
+    lw		$t3, keys($t4)		# $t3 = count[input[i]]
+    addi    $t3, $t3, -1        # $t3 -= 1
+    sw      $t3, keys($t4)
+    addi    $t0, $t0, 1         # for(..., ..., i++)
+    bne     $t0, $t7, buildres
+
+print:
+    li      $v0, 4
+    la      $a0, output_lead
+    syscall
+    li	    $v0, 11
+    li		$a0, 10
+    syscall
 
 end:
     li      $2, 10
